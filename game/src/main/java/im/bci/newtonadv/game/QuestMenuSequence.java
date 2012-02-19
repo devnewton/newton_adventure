112387d4e9605cb1a70adf319c8443346154e07a
devnewton <devnewton>
2012-02-19 22:14:55 +0000

    add hades quest with first level

--- game/src/main/java/im/bci/newtonadv/game/QuestMenuSequence.java
+++ game/src/main/java/im/bci/newtonadv/game/QuestMenuSequence.java
@@ -32,80 +32,97 @@
 package im.bci.newtonadv.game;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import im.bci.newtonadv.Game;
 import im.bci.newtonadv.platform.interfaces.ITrueTypeFont;
 
 /**
- *
+ * 
  * @author devnewton
  */
 public class QuestMenuSequence extends MenuSequence {
 
-    List<QuestSequence> quests;
-    ITrueTypeFont questNameFont;
-    static final int QUEST_MINIATURE_BY_ROW = 2;
-    static final int QUEST_MINIATURE_BY_COLUMN = 2;
-    static final float QUEST_MINIATURE_SPACING = 60;
-    static final float QUEST_MINIATURE_WIDTH = (ortho2DRight - QUEST_MINIATURE_SPACING * (QUEST_MINIATURE_BY_ROW + 1)) / QUEST_MINIATURE_BY_ROW;
-    public static final float QUEST_MINIATURE_HEIGHT = (ortho2DBottom - QUEST_MINIATURE_SPACING * (QUEST_MINIATURE_BY_COLUMN + 1)) / QUEST_MINIATURE_BY_COLUMN;
+	List<QuestSequence> quests;
+	ITrueTypeFont questNameFont;
+	static final int QUEST_MINIATURE_ON_X = 2;
+	static final int QUEST_MINIATURE_ON_Y = 3;
+	static final float QUEST_MINIATURE_SPACING = 60;
+	static final float QUEST_MINIATURE_WIDTH = (ortho2DRight - QUEST_MINIATURE_SPACING
+			* (QUEST_MINIATURE_ON_X + 1))
+			/ QUEST_MINIATURE_ON_X;
+	public static final float QUEST_MINIATURE_HEIGHT = (ortho2DBottom - QUEST_MINIATURE_SPACING
+			* (QUEST_MINIATURE_ON_Y + 1))
+			/ QUEST_MINIATURE_ON_Y;
 
-    public QuestMenuSequence(Game game) {
-        super(game);
-        verticalIncrement = QUEST_MINIATURE_BY_ROW;
-        loadQuests();
-    }
+	public QuestMenuSequence(Game game) {
+		super(game);
+		verticalIncrement = QUEST_MINIATURE_ON_X;
+		loadQuests();
+	}
 
-    @Override
-    public void start() {
-        super.start();
-        questNameFont = game.getView().createQuestNameFont();
-    }
+	@Override
+	public void start() {
+		super.start();
+		questNameFont = game.getView().createQuestNameFont();
+	}
 
-    @Override
-    public void stop() {
-        super.stop();
-        questNameFont.destroy();
-    }
+	@Override
+	public void stop() {
+		super.stop();
+		questNameFont.destroy();
+	}
 
-    private void loadQuests() {
-        quests = new ArrayList<QuestSequence>();
+	private void loadQuests() {
+		quests = new ArrayList<QuestSequence>();
 
-        List<String> questNames = game.getData().listQuests();
-        for (int i = 0; i < questNames.size(); ++i) {
-            String questName = questNames.get(i);
-            QuestSequence questSequence = new QuestSequence(game, questName);
-            quests.add(questSequence);
-            createQuestButton(i, questName, questSequence);
-        }
-    }
+		Iterator<String> questNamesIterator = game.getData().listQuests()
+				.iterator();
 
-    public void setNextSequence(Sequence sequence) {
-        for (QuestSequence quest : quests) {
-            quest.setNextSequence(sequence);
-        }
-    }
+		for (int j = 0; j < QUEST_MINIATURE_ON_Y; ++j) {
+			for (int i = 0; i < QUEST_MINIATURE_ON_X; ++i) {
+				if (!questNamesIterator.hasNext())
+					return;
+				String questName = questNamesIterator.next();
+				QuestSequence questSequence = new QuestSequence(game, questName);
+				quests.add(questSequence);
+				createQuestButton(i, j, questName, questSequence);
+			}
+		}
+	}
 
-    private void createQuestButton(int i, final String questName, final QuestSequence questSequence) {
-        Button questButton = new Button() {
+	public void setNextSequence(Sequence sequence) {
+		for (QuestSequence quest : quests) {
+			quest.setNextSequence(sequence);
+		}
+	}
 
-            @Override
-            void activate() throws TransitionException {
-                throw new Sequence.TransitionException(questSequence);
-            }
+	private void createQuestButton(int i, int j, final String questName,
+			final QuestSequence questSequence) {
+		Button questButton = new Button() {
 
-            @Override
-            public void draw() {
-                game.getView().drawQuestMenuButton(this, questNameFont, questName);
-            }
-        };
-        questButton.offTexture = game.getData().getQuestFile(questName, "bt-quest-off.jpg");
-        questButton.onTexture = game.getData().getQuestFile(questName, "bt-quest-on.jpg");
+			@Override
+			void activate() throws TransitionException {
+				throw new Sequence.TransitionException(questSequence);
+			}
 
-        questButton.x = QUEST_MINIATURE_SPACING + (i % QUEST_MINIATURE_BY_COLUMN) * (QUEST_MINIATURE_WIDTH + QUEST_MINIATURE_SPACING);
-        questButton.y = QUEST_MINIATURE_SPACING + (i / QUEST_MINIATURE_BY_ROW) * (QUEST_MINIATURE_HEIGHT + QUEST_MINIATURE_SPACING);
-        questButton.w = QUEST_MINIATURE_WIDTH;
-        questButton.h = QUEST_MINIATURE_HEIGHT;
-        addButton(questButton);
-    }
+			@Override
+			public void draw() {
+				game.getView().drawQuestMenuButton(this, questNameFont,
+						questName);
+			}
+		};
+		questButton.offTexture = game.getData().getQuestFile(questName,
+				"bt-quest-off.jpg");
+		questButton.onTexture = game.getData().getQuestFile(questName,
+				"bt-quest-on.jpg");
+
+		questButton.x = QUEST_MINIATURE_SPACING + i
+				* (QUEST_MINIATURE_WIDTH + QUEST_MINIATURE_SPACING);
+		questButton.y = QUEST_MINIATURE_SPACING + j
+				* (QUEST_MINIATURE_HEIGHT + QUEST_MINIATURE_SPACING);
+		questButton.w = QUEST_MINIATURE_WIDTH;
+		questButton.h = QUEST_MINIATURE_HEIGHT;
+		addButton(questButton);
+	}
 }
