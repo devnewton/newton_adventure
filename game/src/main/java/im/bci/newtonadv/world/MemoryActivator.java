1ab621aad53033910044817ae78e3de0970bb9e9
devnewton <devnewton>
2012-03-17 13:01:57 +0000

    add MemoryActivator class (to be tested)

---
+++ game/src/main/java/im/bci/newtonadv/world/MemoryActivator.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2009-2010 devnewton <devnewton@bci.im>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'devnewton <devnewton@bci.im>' nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package im.bci.newtonadv.world;
+
+import im.bci.newtonadv.game.FrameTimeInfos;
+import im.bci.newtonadv.game.Updatable;
+import im.bci.newtonadv.platform.interfaces.ITexture;
+import net.phys2d.raw.Body;
+import net.phys2d.raw.BodyList;
+
+class MemoryActivator extends Platform implements Updatable {
+    private final int activableId;
+    private final ITexture onTexture, offTexture, hiddenTexture;
+    private State state = State.HIDDEN;
+    private long showEndTime = -1;
+    private static final long showDuration = 1000000000L;
+    
+    enum State {
+    	HIDDEN,
+    	SHOW,
+    	ACTIVATED
+    }
+
+    MemoryActivator(World world, int activableId, ITexture onTexture, ITexture offTexture, ITexture hiddenTexture) {
+        super(world);
+        this.activableId = activableId;
+        this.offTexture = offTexture;
+        this.onTexture = onTexture;
+        this.hiddenTexture = hiddenTexture;
+        this.setTexture(hiddenTexture);
+    }
+
+    @Override
+    public strictfp void collided(Body body) {
+        if (state != State.HIDDEN) {
+            if (body instanceof Hero || body instanceof Key) {
+            	state = State.SHOW;
+            	setTexture(offTexture);
+            	if(areAllShown()) {
+            		activate();
+            	}
+            }
+        }
+    }
+    
+    private boolean areAllShown() {
+    	BodyList bodies = world.getBodies();
+    	for (int i = 0; i < bodies.size(); ++i) {
+    		Body b = bodies.get(i);
+            if (b instanceof MemoryActivator) {
+            	MemoryActivator m = (MemoryActivator) b;
+            	if(m.activableId == activableId && m.state != State.SHOW)
+            		return false;
+            }
+    	}
+    	return true;
+	}
+
+	void activate() {
+        BodyList bodies = world.getBodies();
+        for (int i = 0; i < bodies.size(); ++i) {
+            Body b = bodies.get(i);
+            if (b instanceof Blocker) {
+                Blocker a = (Blocker) b;
+                if (a.getActivableId() == activableId) {
+                    a.activate();
+                    this.setTexture(onTexture);
+                }
+            } else  if (b instanceof MemoryActivator) {
+            	MemoryActivator m = (MemoryActivator) b;
+            	if(m.activableId == activableId) {
+            		m.state = State.ACTIVATED;
+            		m.setTexture(m.onTexture);
+            	}
+            }
+        }
+    }
+
+	@Override
+	public void update(FrameTimeInfos frameTimeInfos) throws GameOverException {
+		if(state == State.SHOW) {
+			if(showEndTime < 0)
+				showEndTime = frameTimeInfos.currentTime + showDuration;
+			else if(showEndTime > frameTimeInfos.currentTime){
+				state = State.HIDDEN;
+				showEndTime = -1;
+				setTexture(hiddenTexture);
+			}
+		}
+		
+	}
+}
