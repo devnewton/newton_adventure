262757726ef6435c289272ddd83e42a3934be18d
bob <bob>
2013-03-14 23:39:56 +0000

    add mod retro

---
+++ mods/retro/data/quests/retro/retro.tsx
@@ -0,0 +1,351 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset firstgid="0">
+ <tile id="0">
+  <image source="retro_0.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <image source="retro_1.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="hero"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <image source="retro_2.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="apple"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <image source="retro_3.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="key"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <image source="retro_4.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="door"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <image source="retro_5.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cloud"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <image source="retro_6.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_up"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <image source="retro_7.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_down"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <image source="retro_8.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_left"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <image source="retro_9.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_right"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <image source="retro_10.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="mobile_pike_anchor"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <image source="retro_11.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="axe_anchor"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <image source="retro_12.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_up"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <image source="retro_13.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_right"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <image source="retro_14.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_down"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <image source="retro_15.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_left"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <image source="retro_16.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="down_right_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <image source="retro_17.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="down_left_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <image source="retro_18.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="up_left_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <image source="retro_19.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="up_right_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <image source="retro_20.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="mummy"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <image source="retro_21.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="bat"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <image source="retro_22.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <image source="retro_23.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <image source="retro_24.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <image source="retro_25.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="up_right_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <image source="retro_26.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="down_right_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <image source="retro_27.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="down_left_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <image source="retro_28.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="up_left_half_platform"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <image source="retro_29.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <image source="retro_30.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <image source="retro_31.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <image source="retro_32.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <image source="retro_33.png"/>
+  <properties>
+   <property name="newton_adventure.moving_platform.a.x" value="0"/>
+   <property name="newton_adventure.moving_platform.a.y" value="-2"/>
+   <property name="newton_adventure.moving_platform.b.x" value="0"/>
+   <property name="newton_adventure.moving_platform.b.y" value="2"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <image source="retro_34.png"/>
+  <properties>
+   <property name="newton_adventure.moving_platform.a.x" value="-2"/>
+   <property name="newton_adventure.moving_platform.a.y" value="0"/>
+   <property name="newton_adventure.moving_platform.b.x" value="2"/>
+   <property name="newton_adventure.moving_platform.b.y" value="0"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <image source="retro_35.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="blocker1"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <image source="retro_36.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="blocker2"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <image source="retro_37.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="blocker3"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <image source="retro_38.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="door_to_bonus_world"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <image source="retro_39.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="world_map"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <image source="retro_40.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="compass"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <image source="retro_41.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="bomb"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <image source="retro_42.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="bomb_hole"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <image source="retro_43.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="crate"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <image source="retro_44.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="activator1"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <image source="retro_45.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="activator2"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <image source="retro_46.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="activator3"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <image source="retro_47.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="memory_activator1"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <image source="retro_48.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="memory_activator2"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <image source="retro_49.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="memory_activator3"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <image source="retro_50.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="bounce_platform"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <image source="retro_51.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="keylock"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <image source="retro_52.png"/>
+  <properties>
+   <property name="newton_adventure.teleporter.color" value="blue"/>
+   <property name="newton_adventure.type" value="teleporter"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <image source="retro_53.png"/>
+  <properties>
+   <property name="newton_adventure.teleporter.color" value="green"/>
+   <property name="newton_adventure.type" value="teleporter"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <image source="retro_54.png"/>
+  <properties>
+   <property name="newton_adventure.teleporter.color" value="red"/>
+   <property name="newton_adventure.type" value="teleporter"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <image source="retro_55.png"/>
+  <properties>
+   <property name="newton_adventure.teleporter.color" value="yellow"/>
+   <property name="newton_adventure.type" value="teleporter"/>
+  </properties>
+ </tile>
+</tileset>
