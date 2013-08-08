5a70d7758a502c6635ba53751be428cb9c9aa8d7
devnewton <devnewton>
2013-08-08 20:15:25 +0000

    start cute mod

---
+++ mods/cute/data/quests/castle/castle.tsx
@@ -0,0 +1,586 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset firstgid="0">
+ <tile id="0">
+  <image source="castle_0.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <image source="castle_1.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="hero"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <image source="castle_2.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="apple"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <image source="castle_3.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="key"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <image source="castle_4.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="door"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <image source="castle_5.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cloud"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <image source="castle_6.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_up"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <image source="castle_7.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_down"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <image source="castle_8.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_left"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <image source="castle_9.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="pikes_right"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <image source="castle_10.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="mobile_pike_anchor"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <image source="castle_11.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="axe_anchor"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <image source="castle_12.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_up"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <image source="castle_13.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_right"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <image source="castle_14.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_down"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <image source="castle_15.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="cannon_left"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <image source="castle_16.png"/>
+  <properties>
+   <property name="newton_adventure.friction" value="0"/>
+   <property name="newton_adventure.type" value="slash_platform"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <image source="castle_17.png"/>
+  <properties>
+   <property name="newton_adventure.friction" value="0"/>
+   <property name="newton_adventure.type" value="antislash_platform"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <image source="castle_18.png"/>
+  <properties>
+   <property name="newton_adventure.friction" value="0"/>
+   <property name="newton_adventure.type" value="slash_platform"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <image source="castle_19.png"/>
+  <properties>
+   <property name="newton_adventure.friction" value="0"/>
+   <property name="newton_adventure.type" value="antislash_platform"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <image source="castle_20.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="mummy"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <image source="castle_21.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="bat"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <image source="castle_22.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="blue"/>
+   <property name="newton_adventure.moving_platform.a.x" value="-2"/>
+   <property name="newton_adventure.moving_platform.a.y" value="0"/>
+   <property name="newton_adventure.moving_platform.b.x" value="2"/>
+   <property name="newton_adventure.moving_platform.b.y" value="0"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <image source="castle_23.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="blue"/>
+   <property name="newton_adventure.moving_platform.a.x" value="0"/>
+   <property name="newton_adventure.moving_platform.a.y" value="-2"/>
+   <property name="newton_adventure.moving_platform.b.x" value="0"/>
+   <property name="newton_adventure.moving_platform.b.y" value="2"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <image source="castle_24.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <image source="castle_25.png"/>
+ </tile>
+ <tile id="26">
+  <image source="castle_26.png"/>
+ </tile>
+ <tile id="27">
+  <image source="castle_27.png"/>
+ </tile>
+ <tile id="28">
+  <image source="castle_28.png"/>
+ </tile>
+ <tile id="29">
+  <image source="castle_29.png"/>
+ </tile>
+ <tile id="30">
+  <image source="castle_30.png"/>
+ </tile>
+ <tile id="31">
+  <image source="castle_31.png"/>
+ </tile>
+ <tile id="32">
+  <image source="castle_32.png"/>
+ </tile>
+ <tile id="33">
+  <image source="castle_33.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="red"/>
+   <property name="newton_adventure.gfx" value="red_colorizer.nanim"/>
+   <property name="newton_adventure.type" value="colorizer"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <image source="castle_34.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="green"/>
+   <property name="newton_adventure.gfx" value="green_colorizer.nanim"/>
+   <property name="newton_adventure.type" value="colorizer"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <image source="castle_35.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="blue"/>
+   <property name="newton_adventure.gfx" value="blue_colorizer.nanim"/>
+   <property name="newton_adventure.type" value="colorizer"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <image source="castle_36.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="green"/>
+   <property name="newton_adventure.moving_platform.a.x" value="-2"/>
+   <property name="newton_adventure.moving_platform.a.y" value="0"/>
+   <property name="newton_adventure.moving_platform.b.x" value="2"/>
+   <property name="newton_adventure.moving_platform.b.y" value="0"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <image source="castle_37.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="green"/>
+   <property name="newton_adventure.moving_platform.a.x" value="0"/>
+   <property name="newton_adventure.moving_platform.a.y" value="-2"/>
+   <property name="newton_adventure.moving_platform.b.x" value="0"/>
+   <property name="newton_adventure.moving_platform.b.y" value="2"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <image source="castle_38.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="door_to_bonus_world"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <image source="castle_39.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="world_map"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <image source="castle_40.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="compass"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <image source="castle_41.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="bomb"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <image source="castle_42.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="bomb_hole"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <image source="castle_43.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="crate"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <image source="castle_44.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="red"/>
+   <property name="newton_adventure.moving_platform.a.x" value="-2"/>
+   <property name="newton_adventure.moving_platform.a.y" value="0"/>
+   <property name="newton_adventure.moving_platform.b.x" value="2"/>
+   <property name="newton_adventure.moving_platform.b.y" value="0"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <image source="castle_45.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="red"/>
+   <property name="newton_adventure.moving_platform.a.x" value="0"/>
+   <property name="newton_adventure.moving_platform.a.y" value="-2"/>
+   <property name="newton_adventure.moving_platform.b.x" value="0"/>
+   <property name="newton_adventure.moving_platform.b.y" value="2"/>
+   <property name="newton_adventure.type" value="dangerous_moving_platform"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <image source="castle_46.png"/>
+ </tile>
+ <tile id="47">
+  <image source="castle_47.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <image source="castle_48.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <image source="castle_49.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <image source="castle_50.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <image source="castle_51.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <image source="castle_52.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <image source="castle_53.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <image source="castle_54.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <image source="castle_55.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <image source="castle_56.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <image source="castle_57.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <image source="castle_58.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <image source="castle_59.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <image source="castle_60.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <image source="castle_61.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <image source="castle_62.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <image source="castle_63.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <image source="castle_64.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <image source="castle_65.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <image source="castle_66.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <image source="castle_67.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <image source="castle_68.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <image source="castle_69.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <image source="castle_70.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <image source="castle_71.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="blue"/>
+   <property name="newton_adventure.type" value="colored_platform"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <image source="castle_72.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="green"/>
+   <property name="newton_adventure.type" value="colored_platform"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <image source="castle_73.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="red"/>
+   <property name="newton_adventure.type" value="colored_platform"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <image source="castle_74.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="blue"/>
+   <property name="newton_adventure.type" value="cloud"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <image source="castle_75.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="green"/>
+   <property name="newton_adventure.type" value="cloud"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <image source="castle_76.png"/>
+  <properties>
+   <property name="newton_adventure.color" value="red"/>
+   <property name="newton_adventure.type" value="cloud"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <image source="castle_77.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="activator1"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <image source="castle_78.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="activator2"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <image source="castle_79.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="activator3"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <image source="castle_80.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="memory_activator1"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <image source="castle_81.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="memory_activator2"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <image source="castle_82.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="memory_activator3"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <image source="castle_83.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="blocker1"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <image source="castle_84.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="blocker2"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <image source="castle_85.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="blocker3"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <image source="castle_86.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="keylock"/>
+   <property name="newton_adventure.color" value="blue"/>
+  </properties>
+ </tile>
+<tile id="87">
+  <image source="castle_87.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="keylock"/>
+   <property name="newton_adventure.color" value="green"/>
+  </properties>
+ </tile>
+<tile id="88">
+  <image source="castle_88.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="keylock"/>
+   <property name="newton_adventure.color" value="red"/>
+  </properties>
+ </tile>
+<tile id="89">
+  <image source="castle_89.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="keylock"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <image source="castle_90.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="flag"/>
+   <property name="newton_adventure.color" value="blue"/>
+  </properties>
+ </tile>
+<tile id="91">
+  <image source="castle_91.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="flag"/>
+   <property name="newton_adventure.color" value="green"/>
+  </properties>
+ </tile>
+<tile id="92">
+  <image source="castle_92.png"/>
+  <properties>
+   <property name="newton_adventure.type" value="flag"/>
+   <property name="newton_adventure.color" value="red"/>
+  </properties>
+ </tile>
+</tileset>
