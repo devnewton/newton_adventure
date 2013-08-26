1bc71108df5fe1d3d959a038a1ad71ced44033a8
bob <bob>
2013-08-26 16:02:21 +0000

    release 1.11

--- game/release.sh
+++ game/release.sh
@@ -2,10 +2,9 @@
 newton_adventure_version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)'`
 rm newton_adventure-${newton_adventure_version}-installer.exe
 rm newton_adventure-${newton_adventure_version}-installer.jar
-mvn clean package -Pizpack,deb,rpm,rpm32,src
+mvn clean package -Pizpack,rpm,rpm32,src
 cp target/newton_adventure_${newton_adventure_version}_sources.zip ../www/downloads/
 cp target/newton_adventure-${newton_adventure_version}-installer.jar ../www/downloads/
-cp target/newton_adventure_${newton_adventure_version}.deb ../www/downloads/
 cp target/rpm/newton_adventure/RPMS/i686/newton_adventure-${newton_adventure_version}-1.i686.rpm ../www/downloads/
 cp target/newton_adventure-${newton_adventure_version}-installer.jar ./
 mvn install -Pwin
