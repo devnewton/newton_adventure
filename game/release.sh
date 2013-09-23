fd2033523d468ae92266d73827fc1d529d8c0a44
devnewton <devnewton>
2013-09-23 15:17:59 +0000

    simplify build

--- game/release.sh
+++ game/release.sh
@@ -2,7 +2,7 @@
 newton_adventure_version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)'`
 rm newton_adventure-${newton_adventure_version}-installer.exe
 rm newton_adventure-${newton_adventure_version}-installer.jar
-mvn clean package -Pizpack,rpm,rpm32,src,not_deb
+mvn clean package -Pizpack,rpm,rpm32,src
 cp target/newton_adventure_${newton_adventure_version}_sources.zip ../www/downloads/
 cp target/newton_adventure-${newton_adventure_version}-installer.jar ../www/downloads/
 cp target/rpm/newton_adventure/RPMS/i686/newton_adventure-${newton_adventure_version}-1.i686.rpm ../www/downloads/
@@ -11,5 +11,5 @@ mvn install -Pwin
 cp newton_adventure-${newton_adventure_version}-installer.exe  ../www/downloads/
 mvn package -Prpm,rpm64
 cp target/rpm/newton_adventure/RPMS/x86_64/newton_adventure-${newton_adventure_version}-1.x86_64.rpm ../www/downloads/
-mvn package -Pdeb,!not_deb
+mvn package -Pdeb
 cp target/newton_adventure_${newton_adventure_version}.deb ../www/downloads/newton-adventure_${newton_adventure_version}.deb
