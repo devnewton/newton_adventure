a0d3fc6d5f2198501fde7d10319cffb2fb517650
devnewton <devnewton@bci.im>
2014-01-05 00:54:05 +0100

    add safe launcher desktop shortcuts

--- packages/deb/src/launchers/run-newton-adventure.sh
+++ packages/deb/src/launchers/run-newton-adventure.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ -e /usr/lib/jvm/java-7-openjdk-`/usr/share/jarwrapper/java-arch.sh`/bin/java ]
 then
-	/usr/lib/jvm/java-7-openjdk-`/usr/share/jarwrapper/java-arch.sh`/bin/java -jar /opt/newton-adventure/newton-adventure-lwjgl-${project.version}.jar
+	/usr/lib/jvm/java-7-openjdk-`/usr/share/jarwrapper/java-arch.sh`/bin/java -jar /opt/newton-adventure/newton-adventure-lwjgl-${project.version}.jar $*
 else
-/opt/newton-adventure/newton-adventure-lwjgl-${project.version}.jar
+/opt/newton-adventure/newton-adventure-lwjgl-${project.version}.jar $*
 fi
