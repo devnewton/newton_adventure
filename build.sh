0e8a8a3c4177f96ffef65d5628d37815d38d718b
devnewton <devnewton@bci.im>
2014-11-10 10:37:51 +0100

    fix generic installer default dir

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn clean package -Pwin,deb,rpm
+mvn package -Pwin,deb,rpm
 mkdir -p target/release
 cp packages/generic/target/newton-adventure-installer*.jar target/release/
 cp packages/win/target/newton-adventure-installer*.exe target/release/
