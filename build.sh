e57071a37929c7041f21203ad13bf45dcf5279ed
devnewton <devnewton@bci.im>
2014-11-10 11:09:21 +0100

    release 1.16

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ cp packages/generic/target/newton-adventure-installer*.jar target/release/
 cp packages/win/target/newton-adventure-installer*.exe target/release/
 cp packages/deb/target/newton-adventure*.deb target/release/
 cp packages/rpm/target/rpm/newton-adventure/RPMS/*/newton-adventure-*.rpm target/release/
+cp mods/bald/target/newton-adventure-mod-bald*-installer.jar target/release/
+cp mods/duchatelet/target/newton-adventure-mod-duchatelet*-installer.jar target/release/
+cp mods/retro/target/newton-adventure-mod-retro*-installer.jar target/release/
