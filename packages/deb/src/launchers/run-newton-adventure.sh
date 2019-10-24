000f5557578a5a6ff2479e0c5a1cf92a2570abb9
devnewton <devnewton@bci.im>
2019-10-24 23:32:41 +0200

    remove unmaintenable packages

--- packages/deb/src/launchers/run-newton-adventure.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-if [ -e /usr/lib/jvm/java-7-openjdk-`/usr/share/jarwrapper/java-arch.sh`/bin/java ]
-then
-	/usr/lib/jvm/java-7-openjdk-`/usr/share/jarwrapper/java-arch.sh`/bin/java -jar /opt/newton-adventure/newton-adventure-lwjgl-${project.version}.jar $*
-else
-/opt/newton-adventure/newton-adventure-lwjgl-${project.version}.jar $*
-fi
--- packages/generic/src/launchers/run_windows.bat
+++
@@ -1,1 +0,0 @@
-java -jar newton-adventure-lwjgl-${project.version}.jar
